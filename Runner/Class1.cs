fe64d04e6fc03fe916a35d83e9a758a8e357f9be
68681395 <happycoder@foxmail.com>
2016-06-27 17:59:42 +0800

    add

---
+++ Runner/Class1.cs
@@ -0,0 +1,78 @@
+﻿namespace BetterRunAs
+{
+    using CommandLine;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Security;
+    using System.Text;
+    interface IOptions
+    {
+        [Option('u', "user",
+            SetName = "byUser",
+            HelpText = "以此账号运行程序，账号可包含域名")]
+       string User { get; set; }
+
+        [Option('p', "password",
+            SetName = "byUser",
+            HelpText = "账号密码，如果仅提升为管理员权限请留空")]
+        string Password { get; set; }
+
+
+        [CommandLine.Text.Usage]
+        string Useage { get; set; }
+
+        [Value(0, MetaName = "exe file full path",
+            HelpText = "执行文件",
+            Required = true)]
+        string FileName { get; set; }
+    }
+
+  
+    class HeadOptions : IOptions
+    {
+        public string User { get; set; }
+
+        public string Password { get; set; }
+
+        public string FileName { get; set; }
+         
+        public string Useage { get; set; }
+    }
+
+    
+
+
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+
+            Func<IOptions, string> header = opts =>
+            { 
+                var builder = new StringBuilder("Reading ");
+
+                builder.AppendLine(opts.FileName).AppendLine(opts.User).AppendLine(opts.Password);
+
+                builder.AppendLine(opts.Useage);
+                return builder.ToString();
+            };
+
+            var result = Parser.Default.ParseArguments<HeadOptions>(args);
+            var texts = result.MapResult(header,_=> MakeError());
+                
+             
+
+            return texts.Equals(MakeError()) ? 1 : 0;
+        }
+
+        private static object MakeError()
+        {
+            return string.Empty;
+        }
+    }
+
+
+}
---
+++ Runner/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner
+{
+    using System.Diagnostics;
+    using System.Security;
+
+    class Program
+    {
+        private static SecureString buildSecureString(string strIn)
+        {
+            SecureString str = new SecureString();
+            foreach (char ch in strIn.ToCharArray())
+            {
+                str.AppendChar(ch);
+            }
+            str.MakeReadOnly();
+            return str;
+        }
+        static void _Main(string[] args)
+        {
+
+            try
+            {
+                if (args.Length == 4)
+                {
+                    Process process = new Process
+                    {
+                        StartInfo = new ProcessStartInfo(args[0])
+                    };
+                    process.StartInfo.Domain = args[1];
+                    process.StartInfo.UserName = args[2];
+                    process.StartInfo.Password = buildSecureString(args[3]);
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.LoadUserProfile = true;
+                    process.Start();
+                }
+                else
+                {
+                    //  MessageBox.Show("Usage: BetterRunAs.exe \"application\" \"domain\" \"userid\" \"password\"");
+                }
+            }
+            catch (Exception exception)
+            {
+                // MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
